c97de75a28036c698ad2912ba0f6fca7c4d5be19
Agung-N-Pangestu <136967665+Agung-N-Pangestu@users.noreply.github.com>
2023-11-29 13:24:14 +0700

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,14 @@
+select * from bokem
+
+select NIM from bokem
+
+select bokem.Nama from bokem
+
+select bokem.Nama, bokem.NIM from bokem
+
+select b.Nama, b.NIM from bokem b
+
+select Nama As Name from bokem
+
+select Nama As Name, NIM as nim, Asal As City from bokem
+
